7727c72366f503c46a295ffc80104be069298809
Zack Martin <zack@Zacks-MacBook-Air.local>
2019-02-13 06:41:34 -0500

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://zmartin:zmartin02@ds229771.mlab.com:29771/devconnector02"
+  mongoURI:
+    "mongodb://zmartin:zmartin02@ds229771.mlab.com:29771/devconnector02",
+  secretOrKey: "secret"
 };
